1b6a16987661205b07146b5c4208e3f09448baf1
majahede <majahedegard@gmail.com>
2025-01-11 14:13:01 +0100

    add endpoints to controller

--- BookApi/Controllers/BookController.cs
+++ BookApi/Controllers/BookController.cs
@@ -15,9 +15,45 @@ namespace BookApi.Controllers
         }
 
         [HttpGet]
-        public async Task<IEnumerable<Book>> GetAll()
+        public async Task<ActionResult<IEnumerable<Book>>> GetAll()
         {
-            return await _bookService.GetAllBooks();
+            var books = await _bookService.GetAllBooks();
+            return Ok(books);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<Book>> GetById(Guid id)
+        {
+            var book = await _bookService.GetBookById(id);
+
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Book book)
+        {
+            var id = await _bookService.CreateBook(book);
+
+            return Ok(id);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> Update(Guid id, [FromBody] Book book)
+        {
+            var updatedBook = await _bookService.UpdateBook(id, book);
+
+            return Ok(updatedBook);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            await _bookService.DeleteBook(id);
+
+            return Ok("Book was deleted successfully");
         }
     }
 }
--- BookApi/Interfaces/IBookService.cs
+++ BookApi/Interfaces/IBookService.cs
@@ -4,6 +4,10 @@ namespace BookApi.Interfaces
 {
     public interface IBookService
     {
+        Task<string> CreateBook(Book book);
+        Task DeleteBook(Guid id);
         Task<IEnumerable<Book>> GetAllBooks();
+        Task<Book> GetBookById(Guid id);
+        Task<Book> UpdateBook(Guid id, Book book);
     }
 }
